f47992ba5f0f0947801325227d291607707e3241
pwolfteich <paul.wolfteich@gmail.com>
2016-04-05 19:56:01 -0400

    Added ranged combat.  Peter fires a sample image when shift is pushed.  Also made sure displayObjectContainer's children arrayList of DisplayObjectContainers instead of DisplayObjects, so that objects will now call their own childrens update functions.

---
+++ asmelee/ASMelee/src/edu/virginia/engine/display/RangedAttack.java
@@ -0,0 +1,65 @@
+package edu.virginia.engine.display;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class RangedAttack extends AnimatedSprite{
+
+	public Character myCharacter;
+	public int xSpeed;
+	public int ySpeed;
+	public int damage;
+	public int knockback;
+	public RangedAttack(Character c) //creates a ranged attack in front of the character that moves at the set speed
+	{
+		super(c.getId()+"_projectile",false);
+		myCharacter = c;
+		c.getParent().addChild(this);
+		this.setImage("sample_ranged.png");
+		ySpeed = 0;
+		xSpeed = 1;
+		damage = 5;
+		knockback = 50;
+		int xoffset =(int)(c.getUnscaledWidth()*c.getScaleX());
+		if(c.getScaleX() < 0)
+		{
+			xSpeed *=-1;
+			xoffset*=3;
+		}
+		this.setPosition(new Point((int)c.getGlobalPosition().x+xoffset,(int)c.getGlobalPosition().y));
+	}
+	public void update(ArrayList<String> pressedKeys)
+	{
+		super.update(pressedKeys);
+		Point p=new Point();
+		p.setLocation(position.getX()+xSpeed, position.getY()+ySpeed);
+		this.setPosition(p);
+		//System.out.println("updated");
+	}
+	public void destroy(DisplayObject o)
+	{
+		this.setVisible(false);
+		myCharacter.getParent().removeChild(this);
+		if(o instanceof Character)
+		{
+			Character otherChar=(Character)o;
+			otherChar.healthbar.decreaseHealth(damage);
+		}
+	}
+	public void setDamage(int dam)
+	{
+		damage = dam;
+	}
+	public void setKnockback(int knock)
+	{
+		knockback = knock;
+	}
+	public void setYSpeed(int ys)
+	{
+		ySpeed = ys;
+	}
+	public void setXSpeed(int xs)
+	{
+		xSpeed = xs;
+	}
+}
---
+++ asmelee/ASMelee/src/edu/virginia/engine/events/RangedCollisionManager.java
@@ -0,0 +1,87 @@
+package edu.virginia.engine.events;
+
+import java.awt.Point;
+
+import edu.virginia.engine.display.Mario;
+import edu.virginia.engine.display.PhysicsSprite;
+import edu.virginia.engine.display.RangedAttack;
+import edu.virginia.engine.display.Stewie;
+import edu.virginia.main.Main;
+import edu.virginia.engine.display.Character;
+import edu.virginia.engine.tweening.Tween;
+import edu.virginia.engine.tweening.TweenJuggler;
+import edu.virginia.engine.tweening.TweenTransitions;
+import edu.virginia.engine.tweening.TweenableParam;
+import edu.virginia.engine.tweening.TweenTransitions.transitiontype;
+
+public class RangedCollisionManager implements IEventListener {
+	@Override
+	public void handleEvent(Event event) {
+		if(event.getSource() instanceof Character && event instanceof RangedCollisionEvent)
+		{
+			RangedCollisionEvent ce = (RangedCollisionEvent)event;
+			Character other = (Character) event.getSource();
+			System.out.println("event thrown ");
+			 RangedAttack ra= ce.rangedAttack;
+			if(other.equals(ra.myCharacter))
+				return;
+
+			System.out.println("event continued ");
+			/*
+			 * Add all the hitting code in this if statement
+			 */
+			// if character c is hitting other
+		
+			/*
+			 * if direction == 0, we are hitting other character from left
+			 * if direction == 1, we are hitting other character from right
+			 */
+			int direction = 0;
+			if(ra.getPosition().getX()<other.getPosition().getX())
+				direction = 0;
+			else
+				direction = 1;
+			Tween linearTween = new Tween(other, new TweenTransitions(transitiontype.easeOut), "easeOut");
+			int originalX = (int) other.getPosition().getX();
+			int originalY = (int) other.getPosition().getY();
+			
+			int newX = 0;
+			int newY = 0;
+			
+			// add code that depends on move
+			if(other.healthbar.actualHealth > 0){
+				if(direction == 0)
+					newX = originalX + ra.knockback;
+				if(direction == 1)
+					newX = originalX - ra.knockback;
+				
+				linearTween.animate(TweenableParam.POSITIONX, originalX, newX, 500);
+				System.out.println("Mario health is: " + (other.healthbar.getActualHealth()-5));
+				ra.destroy(other);//destroys the object and deals damage to other
+			}
+			else
+			{
+				if(direction == 0)
+				{
+					newX = originalX + 500;
+				}
+				if(direction == 1)
+				{
+					newX = originalX - 500;
+				}
+				
+				newY = originalY - 500;
+				
+				/*
+				 * Clear any other tweens acting upon object
+				 */
+				TweenJuggler.getInstance().clearTweens(other);
+				
+				Tween deathTween = new Tween(other, new TweenTransitions(transitiontype.lineartrans), "LinearTween");
+				deathTween.animate(TweenableParam.POSITIONX, originalY, newX, 1500);
+				deathTween.animate(TweenableParam.POSITIONY, originalY, newY, 1500);
+			
+			}
+		}
+	}
+}
